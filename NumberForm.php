a6545245ff2d13088bf99a4186ef91703cb86da8
Russ LaScala <32844708+Russlascala@users.noreply.github.com>
2018-03-24 13:35:55 -0400

    Add files via upload

---
+++ NumberForm.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Number Form</title>
+</head>
+<body>
+    <?php
+        $DisplayForm = TRUE;
+        $Number = ""; 
+
+        if (isset($_POST["Submit"])) {
+            $Number = $_POST['Number'];
+            if (is_numeric($Number)) {
+                $DisplayForm = FALSE;
+            } else {
+                echo "<p>You need to enter a numeric value.</p>\n<p>Try again..";
+            }
+        }
+
+        if ($DisplayForm) {
+            ?>
+            <form name="NumberForm" action ="NumberForm.php" method = "post">
+            <p>Enter a number: <input type="text" name="Number" value="<?php echo $Number; ?>" /> </p>
+            <p><input type = "reset" value = "Clear Form" />&nbsp;
+            &nbsp;<input type="submit" name="Submit" value ="Send Form"</p>
+            </form>
+            <?php
+        } else {
+            echo "<p>Thank you for entering a number .</p>\n";
+            echo "<p>Your number, $Number, squared is " . ($Number*$Number) . ".</p>\n ";
+            echo "<p><a href=\"NumberForm.php\">Try again?</a></p>\n";
+        }
+    
+    ?>
+</body>
+</html>
